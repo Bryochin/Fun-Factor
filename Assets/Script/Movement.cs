dc94b97f1f5a9f144c30f2244ae1e40d14ebe91e
Bryochin <bfremond@artfx.fr>
2023-03-22 13:10:59 +0100

    First Save

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float turnSpeed = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.Rotate(Vector3.left * turnSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+    }
+}
